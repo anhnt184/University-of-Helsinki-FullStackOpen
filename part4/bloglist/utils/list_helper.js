1bc4a1d19e6d5d23a6e0e584319619aef8c250e1
Anh Nguyen <112703772+anhnt184@users.noreply.github.com>
2023-06-16 00:16:03 +0300

    Added exercises 4.6 and 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,39 @@
+const _ = require('lodash')
+
+// Most likes function
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  const groupedBlogs = _.groupBy(blogs, 'author')
+  
+  const authorLikes = _.mapValues(groupedBlogs, (blogs) => {
+    return _.sumBy(blogs, 'likes')
+  })
+  
+  const maxAuthor = _.maxBy(_.keys(authorLikes), (author) => authorLikes[author])
+  const maxLikes = authorLikes[maxAuthor]
+  
+  return { author: maxAuthor, likes: maxLikes }
+}
+
+// Most blogs function
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  
+  const groupedBlogs = _.groupBy(blogs, 'author')
+  const authorCounts = _.mapValues(groupedBlogs, 'length')
+  const maxAuthor = _.maxBy(_.keys(authorCounts), (author) => authorCounts[author])
+  const maxBlogs = authorCounts[maxAuthor]
+  
+  return {
+    author: maxAuthor,
+    blogs: maxBlogs
+  }
+}
+
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0) {
     return null // Return null if the list is empty
@@ -22,6 +58,8 @@ const dummy = (blogs) => {
 }
   
 module.exports = {
+  mostLikes,
+  mostBlogs,
   favoriteBlog,
   totalLikes,
   dummy
