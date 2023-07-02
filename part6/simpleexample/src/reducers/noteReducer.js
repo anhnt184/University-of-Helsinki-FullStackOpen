60d7d4c7a12e690c0a2d4a4f0309453716566f80
Anh Nguyen <112703772+anhnt184@users.noreply.github.com>
2023-07-03 01:27:13 +0300

    Added Exercises 6.3.-6.8 Part6 Redux

--- part6/simpleexample/src/reducers/noteReducer.js
+++ part6/simpleexample/src/reducers/noteReducer.js
@@ -18,5 +18,27 @@ const noteReducer = (state = [], action) => {
   }
 }
 
-  export default noteReducer
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
+export default noteReducer
+
   
