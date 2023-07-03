773bc8be738f8895eb4e13e989a82a07e5544618
Anh Nguyen <112703772+anhnt184@users.noreply.github.com>
2023-07-03 22:25:13 +0300

    Added Example Part6c Redux thunk

--- part6/simpleexample/src/index.js
+++ part6/simpleexample/src/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createStore, combineReducers } from 'redux'
+// import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
+import store from './store'
 
 import App from './App'
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
+// import noteReducer, { setNotes } from './reducers/noteReducer'
+// import filterReducer from './reducers/filterReducer'
+
+// import noteService from './services/notes'
 
 
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
 
+// const store = configureStore({
+//   reducer: {
+//     notes: noteReducer,
+//     filter: filterReducer
+//   }
+// })
+
+// noteService.getAll().then(notes =>
+//   store.dispatch(setNotes(notes))
+// )
 // console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
