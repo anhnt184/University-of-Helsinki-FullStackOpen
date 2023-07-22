bc0057e733b13a53b8db63518d02e4d826827b10
Anh Nguyen <112703772+anhnt184@users.noreply.github.com>
2023-07-22 06:00:48 +0300

    Added Exercises solutions GraphQL and MongoDB

---
+++ part8-GraphQL/liblary-solution/frontend/src/components/LoginForm.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+import { useMutation } from '@apollo/client'
+import { LOGIN } from '../queries'
+import { STORAGE_KEY } from '../util'
+
+const LoginForm = ({ setError, setToken, setPage, show }) => {
+  const [username, setUsername] = useState('khanhvy')
+  const [password, setPassword] = useState('secret')
+
+  const [ login, result ] = useMutation(LOGIN, {
+    onError: (error) => {
+      setError(error.message)
+    }
+  })
+
+  useEffect(() => {
+    if ( result.data ) {
+      const token = result.data.login.value
+      console.log('token: ', token)
+      setToken(token)
+      setPage('books')
+      localStorage.setItem(STORAGE_KEY, token)
+    }
+  }, [result.data]) // eslint-disable-line
+
+  if (!show) {
+    return null
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+    try {
+    login({ variables: { username, password } })
+  } catch (error) {
+    setError(error.message)
+  }
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          username <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password <input
+            type='password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
